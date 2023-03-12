b66105498b0650632fc7a5d8b056114c7d5c5453
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-03-12 15:33:56 +0100

    basic pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '@pages/home/home.component';
+import { LoginComponent } from '@pages/login/login.component';
+import { PostsComponent } from '@pages/posts/posts.component';
+import { ProjectsComponent } from '@pages/projects/projects.component';
+import { ProjectComponent } from '@pages/project/project.component';
+import { PostComponent } from '@pages/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'posts',
+    component: PostsComponent,
+    children: [{ path: ':id', component: PostComponent }]
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    children: [{ path: ':id', component: ProjectComponent }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from '@components/navigation/navigation.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { LoginComponent } from '@pages/login/login.component';
+import { PostsComponent } from '@pages/posts/posts.component';
+import { ProjectsComponent } from '@pages/projects/projects.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { PostComponent } from './pages/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    LoginComponent,
+    PostsComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    PostComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
