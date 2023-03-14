d778ebdd8ba4c956d19dea8c5a26b65e92e93564
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-03-14 08:28:43 +0100

    authenitcation route protection guard

--- src/app/guards/is-authenticated.guard.ts
+++ src/app/guards/is-authenticated.guard.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '@services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsAuthenticatedGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -19,6 +23,10 @@ export class IsAuthenticatedGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return true;
+    return this.authService.isLoggedIn$.pipe(
+      tap(async (isLoggedIn) => {
+        if (!isLoggedIn) await this.router.navigate(['login']);
+      })
+    );
   }
 }
