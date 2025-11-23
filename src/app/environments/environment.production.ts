1e249083c154be0153bd23e3ba796b906567758b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2025-11-23 13:41:20 +0100

    replace env script

--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'https://api.mikhailbahdashych.me/api',
   staticStorage: 'https://bahdashych-on-security.s3.eu-central-1.amazonaws.com',
   basicAuth: {
-    username: process.env['BASIC_AUTH_USERNAME'],
-    password: process.env['BASIC_AUTH_PASSWORD']
+    username: '${BASIC_AUTH_USERNAME}',
+    password: '${BASIC_AUTH_PASSWORD}'
   }
 };
