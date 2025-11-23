ddb13f26b8c9e455e3a7f4f2d06767f968cac8b4
bl4drnnr <mikhail.bahdashych@protonmail.com>
2025-11-23 14:40:43 +0100

    prod config

--- scripts/generate-env-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat > ./src/assets/env-config.js << EOF
-window.BASIC_AUTH_USERNAME = '${BASIC_AUTH_USERNAME}';
-window.BASIC_AUTH_PASSWORD = '${BASIC_AUTH_PASSWORD}';
-EOF
-
-echo "env-config.js generated successfully"
--- src/app/environments/environment.production.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.mikhailbahdashych.me/api',
-  staticStorage: 'https://bahdashych-on-security.s3.eu-central-1.amazonaws.com',
-  basicAuth: {
-    username: '',
-    password: ''
-  }
-};
